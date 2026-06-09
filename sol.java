c528e2a7b16b62bc46dc792ea60c03c7e65c108e
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-09 21:21:39 +0530

    Update sol.java

--- sol.java
+++ sol.java
@@ -6,7 +6,7 @@ public class Main {
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) return false;
         }
-        return true;
+           return true;
     }
 
     public static void main(String[] args) {
