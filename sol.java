83c41876b43dc64c8e6b5aba0bb627186f7b60b7
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-09 21:19:45 +0530

    Create sol.java

---
+++ sol.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    static boolean isPrime(int num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // given number
+        int count = 0;
+
+        for (int i = 1; i < n; i++) { // exclude 10
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+
+        System.out.println("Count of primes from 1 to " + (n - 1) + " is: " + count);
+    }
+}
